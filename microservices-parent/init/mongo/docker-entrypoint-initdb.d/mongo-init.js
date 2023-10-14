9513702e2cb0b7f703fe7975d39be8d833c5b1d9
VirtAltoKohi <61061541+MRCGInteract@users.noreply.github.com>
2023-10-14 12:20:46 -0400

    Project is completed for ICE2 submission

--- microservices-parent/init/mongo/docker-entrypoint-initdb.d/mongo-init.js
+++ microservices-parent/init/mongo/docker-entrypoint-initdb.d/mongo-init.js
@@ -1,3 +1,18 @@
+print("START");
+
+db = db.getSiblingDB("product-service");
+
+db.createUser(
+    {
+        user: "mongoadmin",
+        pwd: "password",
+        roles: [{role: "readWrite", db: "product-service"}]
+    }
+)
+
+db.createCollection("user");
+
+print("END");
 print("Start");
 
 db = db.getSibling("product-service");
